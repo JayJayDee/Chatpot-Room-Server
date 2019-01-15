2c8cba196a10d3dd9f345f229b7e0e846f203353
JayJayDee <jindongp@gmail.com>
2019-01-15 14:47:29 +0900

    added preparation for authentication/authorization middleware.

--- src/middlewares/modules.ts
+++ src/middlewares/modules.ts
@@ -2,4 +2,5 @@ export namespace MiddlewareModules {
   export const NotFound = 'Middleware/NotFound';
   export const Error = 'Middleware/Error';
   export const Authentication = 'Middleware/Authenticaion';
+  export const Authorization = 'Midleware/Authorization';
 }
--- src/middlewares/types.ts
+++ src/middlewares/types.ts
@@ -4,4 +4,5 @@ export namespace MiddlewareTypes {
   export type NotFound = RequestHandler;
   export type Error = ErrorRequestHandler;
   export type Authentication = RequestHandler;
+  export type Authorization = (memberTokenPath: string[]) => RequestHandler;
 }
