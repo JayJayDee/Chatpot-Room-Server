82a940cde494e3c44b07cbc62e772526e055c9af
JayJayDee <jindongp@gmail.com>
2019-01-16 14:25:28 +0900

    added authEnabled flag to configuration. applied authEnabled:boolean flag to middlewares. applied auth-middleware to GET /my/:token/rooms endpoint.

--- src/endpoints/my-endpoints.ts
+++ src/endpoints/my-endpoints.ts
@@ -1,17 +1,21 @@
 import { injectable } from 'smart-factory';
 import { EndpointModules } from './modules';
 import { EndpointTypes } from './types';
+import { MiddlewareModules, MiddlewareTypes } from '../middlewares';
 
 injectable(EndpointModules.My.Rooms,
-  [ EndpointModules.Utils.WrapAync ],
-  async (wrapAsync: EndpointTypes.Utils.WrapAsync): Promise<EndpointTypes.Endpoint> =>
-    ({
-      uri: '/my/:member_token/rooms',
-      method: EndpointTypes.EndpointMethod.GET,
-      handler: [
-        wrapAsync(async (req, res, next) => {
-          // TODO: to be implemented.
-          res.status(200).json([]);
-        })
-      ]
-    }));
+  [ EndpointModules.Utils.WrapAync,
+    MiddlewareModules.Authorization ],
+  async (wrapAsync: EndpointTypes.Utils.WrapAsync,
+    authorize: MiddlewareTypes.Authorization): Promise<EndpointTypes.Endpoint> =>
+      ({
+        uri: '/my/:member_token/rooms',
+        method: EndpointTypes.EndpointMethod.GET,
+        handler: [
+          authorize(['params', 'member_token']),
+          wrapAsync(async (req, res, next) => {
+            // TODO: to be implemented.
+            res.status(200).json([]);
+          })
+        ]
+      }));
--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -4,45 +4,67 @@ import { MiddlewareModules } from './modules';
 import { MiddlewareTypes } from './types';
 import { UtilModules, UtilTypes } from '../utils';
 import { BaseSecurityError } from '../errors';
+import { ConfigModules, ConfigTypes } from '../configs';
+import { LoggerModules, LoggerTypes } from '../loggers';
 
 class AuthenticationFailError extends BaseSecurityError {}
 class AuthorizationFailError extends BaseSecurityError {}
 
 injectable(MiddlewareModules.Authentication,
-  [ UtilModules.Auth.ValidateSessionKey ],
-  async (validateSessionKey: UtilTypes.Auth.ValidateSessionKey): Promise<MiddlewareTypes.Authentication> =>
-
-    (req, res, next) => {
-      const sessionKey: string = req.query.session_key;
-      if (!sessionKey) return next(new AuthenticationFailError('session_key not found'));
-
-      const validated = validateSessionKey(sessionKey);
-      if (validated.valid === false) return next(new AuthenticationFailError('invalid session_key'));
-      if (validated.expired === true) return next(new AuthenticationFailError('invalid session_key')); // TODO: to be changed to SESSION_EXPIRED
-      next();
-    });
-
-injectable(MiddlewareModules.Authorization,
-  [ UtilModules.Auth.DecryptMemberToken,
+  [ ConfigModules.CredentialConfig,
+    LoggerModules.Logger,
     UtilModules.Auth.ValidateSessionKey ],
-  async (decryptMemberToken: UtilTypes.Auth.DecryptMemberToken,
-    validateSessionKey: UtilTypes.Auth.ValidateSessionKey): Promise<MiddlewareTypes.Authorization> =>
+  async (cfg: ConfigTypes.CredentialConfig,
+    log: LoggerTypes.Logger,
+    validateSessionKey: UtilTypes.Auth.ValidateSessionKey): Promise<MiddlewareTypes.Authentication> =>
 
-    (memberTokenPath: string[]) =>
       (req, res, next) => {
+        if (cfg.authEnabled === false) {
+          log.debug('[auth-middleware] auth-disabled. ignoring authentication.');
+          return next();
+        }
+        log.debug('[auth-middleware] auth-enabled. starting authentication..');
+
         const sessionKey: string = req.query.session_key;
-        const memberToken: string = get(req, memberTokenPath);
-
-        if (!sessionKey) return next(new AuthorizationFailError('session_key not supplied'));
-        if (!memberToken) return next(new AuthorizationFailError('member_token not supplied'));
-
-        const member = decryptMemberToken(memberToken);
-        if (!member) return next(new AuthorizationFailError('invalid member_token'));
+        if (!sessionKey) return next(new AuthenticationFailError('session_key not found'));
 
         const validated = validateSessionKey(sessionKey);
-        if (validated.valid === false) return next(new AuthorizationFailError('invalid session_key'));
-        if (validated.expired === true) return next(new AuthorizationFailError('invalid session_key')); // TODO: to be changed to SESSION_EXPIRED
-
-        if (validated.member_no !== member.member_no) return next(new AuthorizationFailError('not allowed operation'));
+        if (validated.valid === false) return next(new AuthenticationFailError('invalid session_key'));
+        if (validated.expired === true) return next(new AuthenticationFailError('invalid session_key')); // TODO: to be changed to SESSION_EXPIRED
         next();
       });
+
+injectable(MiddlewareModules.Authorization,
+  [ ConfigModules.CredentialConfig,
+    LoggerModules.Logger,
+    UtilModules.Auth.DecryptMemberToken,
+    UtilModules.Auth.ValidateSessionKey ],
+  async (cfg: ConfigTypes.CredentialConfig,
+    log: LoggerTypes.Logger,
+    decryptMemberToken: UtilTypes.Auth.DecryptMemberToken,
+    validateSessionKey: UtilTypes.Auth.ValidateSessionKey): Promise<MiddlewareTypes.Authorization> =>
+
+      (memberTokenPath: string[]) =>
+        (req, res, next) => {
+          if (cfg.authEnabled === false) {
+            log.debug('[auth-middleware] auth-disabled. ignoring authorization.');
+            return next();
+          }
+          log.debug('[auth-middleware] auth-enabled. starting authorization..');
+
+          const sessionKey: string = req.query.session_key;
+          const memberToken: string = get(req, memberTokenPath);
+
+          if (!sessionKey) return next(new AuthorizationFailError('session_key not supplied'));
+          if (!memberToken) return next(new AuthorizationFailError('member_token not supplied'));
+
+          const member = decryptMemberToken(memberToken);
+          if (!member) return next(new AuthorizationFailError('invalid member_token'));
+
+          const validated = validateSessionKey(sessionKey);
+          if (validated.valid === false) return next(new AuthorizationFailError('invalid session_key'));
+          if (validated.expired === true) return next(new AuthorizationFailError('invalid session_key')); // TODO: to be changed to SESSION_EXPIRED
+
+          if (validated.member_no !== member.member_no) return next(new AuthorizationFailError('not allowed operation'));
+          next();
+        });
