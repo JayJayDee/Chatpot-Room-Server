a3057a5560e7de57195baa8026b67d575dbf82df
JayJayDee <jindongp@gmail.com>
2019-07-27 13:58:53 +0900

    added some implementation.

--- src/models/roulette-model.ts
+++ src/models/roulette-model.ts
@@ -1,16 +1,26 @@
+import { createHash } from 'crypto';
 import { injectable } from 'smart-factory';
 import { ModelModules } from './modules';
 import { ModelTypes } from './types';
+import { MysqlModules, MysqlTypes } from '../mysql';
 
 injectable(ModelModules.Roulette.Request,
-  [],
-  async (): Promise<ModelTypes.Roulette.Request> =>
+  [ MysqlModules.Mysql ],
+  async (mysql: MysqlTypes.MysqlDriver): Promise<ModelTypes.Roulette.Request> =>
 
     async (param) => {
-      return null;
+      const requestId = generateRequestId(param.member_no);
+      console.log(requestId);
+      const resp = await mysql.transaction(async (con) => {
+      });
+      return resp;
     });
 
 
 injectable(ModelModules.Roulette.Status,
   [],
   async () => {});
+
+
+const generateRequestId = (memberNo: number): string =>
+  createHash('sha256').update(`${memberNo}${Date.now()}`).digest('hex');
